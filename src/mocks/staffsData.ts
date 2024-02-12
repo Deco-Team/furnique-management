2ab87a2324153c606b85e4c0627bebc15e69055f
sarahnguyen <thanhhao2832@gmail.com>
2024-02-13 00:47:31 +0700

    [UI][HaoNTT] Creat view staff list with mock data

---
+++ src/mocks/staffsData.ts
@@ -0,0 +1,102 @@
+export const staffsRows = [
+  {
+    id: 1,
+    firstName: 'John',
+    lastName: 'Doe',
+    staffCode: 'EF123456',
+    phone: '0987654321',
+    avatar: 'https://i.stack.imgur.com/l60Hf.png',
+    email: 'staff1@gmail.com',
+    role: 'STAFF'
+  },
+  {
+    id: 2,
+    firstName: 'Alice',
+    lastName: 'Smith',
+    staffCode: 'AB987654',
+    phone: '0987654322',
+    avatar: 'https://i.stack.imgur.com/l60Hf.png',
+    email: 'staff2@gmail.com',
+    role: 'DELIVERY_STAFF'
+  },
+  {
+    id: 3,
+    firstName: 'Michael',
+    lastName: 'Johnson',
+    staffCode: 'CD345678',
+    phone: '0987654323',
+    avatar: 'https://i.stack.imgur.com/l60Hf.png',
+    email: 'staff3@gmail.com',
+    role: 'CONSULTANT_STAFF'
+  },
+  {
+    id: 4,
+    firstName: 'Emily',
+    lastName: 'Williams',
+    staffCode: 'GH543210',
+    phone: '0987654324',
+    avatar: 'https://i.stack.imgur.com/l60Hf.png',
+    email: 'staff4@gmail.com',
+    role: 'STAFF'
+  },
+  {
+    id: 5,
+    firstName: 'James',
+    lastName: 'Brown',
+    staffCode: 'JK876543',
+    phone: '0987654325',
+    avatar: 'https://i.stack.imgur.com/l60Hf.png',
+    email: 'staff5@gmail.com',
+    role: 'DELIVERY_STAFF'
+  },
+  {
+    id: 6,
+    firstName: 'Emma',
+    lastName: 'Jones',
+    staffCode: 'LM654321',
+    phone: '0987654326',
+    avatar: 'https://i.stack.imgur.com/l60Hf.png',
+    email: 'staff6@gmail.com',
+    role: 'CONSULTANT_STAFF'
+  },
+  {
+    id: 7,
+    firstName: 'William',
+    lastName: 'Miller',
+    staffCode: 'NO098765',
+    phone: '0987654327',
+    avatar: 'https://i.stack.imgur.com/l60Hf.png',
+    email: 'staff7@gmail.com',
+    role: 'STAFF'
+  },
+  {
+    id: 8,
+    firstName: 'Olivia',
+    lastName: 'Davis',
+    staffCode: 'PQ123456',
+    phone: '0987654328',
+    avatar: 'https://i.stack.imgur.com/l60Hf.png',
+    email: 'staff8@gmail.com',
+    role: 'DELIVERY_STAFF'
+  },
+  {
+    id: 9,
+    firstName: 'Alexander',
+    lastName: 'Wilson',
+    staffCode: 'RS789012',
+    phone: '0987654329',
+    avatar: 'https://i.stack.imgur.com/l60Hf.png',
+    email: 'staff9@gmail.com',
+    role: 'CONSULTANT_STAFF'
+  },
+  {
+    id: 10,
+    firstName: 'Sophia',
+    lastName: 'Martinez',
+    staffCode: 'TU345678',
+    phone: '0987654330',
+    avatar: 'https://i.stack.imgur.com/l60Hf.png',
+    email: 'staff10@gmail.com',
+    role: 'STAFF'
+  }
+]
