b9a8f4308007106627dba5782c14f7716ca2d07a
PhongNH <andyhoaiphong2003@gmail.com>
2024-03-11 09:51:19 +0700

    [Implement][Task] Resolve conflict

--- src/pages/delivery/Delivery.tsx
+++ src/pages/delivery/Delivery.tsx
@@ -1,13 +1,8 @@
 import DeliveryTable from './table/DeliveryTable'
-import CreateDeliveryModal from './modal/CreateDeliveryModal'
-import useAuth from '~/hooks/useAuth'
-import { UserRole } from '~/global/enum'
 
 const Delivery = () => {
-  const { user } = useAuth()
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
-      {user?.role === UserRole.ADMIN || user?.role === UserRole.STAFF ? <CreateDeliveryModal /> : null}
       <DeliveryTable />
     </div>
   )
