98acf3eb892181d9ff6587d3f249a8a9888e99d7
nghiavohuynhdai <nghiavo.developer@gmail.com>
2024-03-19 23:27:22 +0700

    add visit showroom booking tab

---
+++ src/pages/visit-booking/VisitBooking.tsx
@@ -0,0 +1,11 @@
+import VisitBookingTable from './table/VisitBookingTable'
+
+const VisitBooking = () => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}>
+      <VisitBookingTable />
+    </div>
+  )
+}
+
+export default VisitBooking
